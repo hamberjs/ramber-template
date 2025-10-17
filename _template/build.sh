e290856224a4e1ee77ab0f2074d84443638af9e9
NKDuy <kn145660@gmail.com>
2025-10-17 10:42:52 +0700

    Update build.sh

--- _template/build.sh
+++ _template/build.sh
@@ -15,9 +15,5 @@ WEBPACK=webpack
 # force push rollup and webpack branches and repos using HTTPS + token
 # Expected: TOKEN environment variable available
 REPO_MAIN="hamberjs/ramber-template"
-REPO_ROLLUP="hamberjs/ramber-template-rollup"
-REPO_WEBPACK="hamberjs/ramber-template-webpack"
 
 git push "https://$TOKEN@github.com/$REPO_MAIN.git" $ROLLUP $WEBPACK -f
-git push "https://$TOKEN@github.com/$REPO_ROLLUP.git" $ROLLUP:master -f
-git push "https://$TOKEN@github.com/$REPO_WEBPACK.git" $WEBPACK:master -f
